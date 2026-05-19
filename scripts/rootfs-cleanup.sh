7b7595ad1d028befd693bd4eb3c581f4dab2e155
Arnaud Rebillout <arnaudr@kali.org>
2026-05-19 14:04:12 +0700

    Switch to deb822 apt sources format

--- scripts/rootfs-cleanup.sh
+++ scripts/rootfs-cleanup.sh
@@ -2,23 +2,18 @@
 
 ## REF: https://gitlab.com/kalilinux/build-scripts/kali-vm/-/blob/main/scripts/finish-install.sh
 configure_apt_sources_list() {
-    # make sources.list empty, to force setting defaults
-    echo > /etc/apt/sources.list
+    echo "INFO: setting up APT data source kali.sources"
 
-    if grep -q '^deb.*http.kali.org' /etc/apt/sources.list; then
-        echo "INFO: sources.list is configured, everything is fine"
-        return
-    fi
-
-    echo "INFO: setting up Kali's default"
-
-    cat >/etc/apt/sources.list <<END
-# See https://www.kali.org/docs/general-use/kali-linux-sources-list-repositories/
-deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware
-
-# Additional line for source packages
-# deb-src http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware
+    rm -f /etc/apt/sources.list
+    cat  >/etc/apt/sources.list.d/kali.sources <<END
+# See https://www.kali.org/docs/general-use/kali-apt-sources/
+Types: deb
+URIs: http://http.kali.org/kali/
+Suites: kali-rolling
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/kali-archive-keyring.gpg
 END
+
     apt-get update
 }
 configure_apt_sources_list
